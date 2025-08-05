3711bb34638d9f6a4c3ad08a8bfefe4ab44963f8
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-08-05 11:53:08 +0900

    refactor: 레거시 객체 및 테스트 객체 제거

--- src/main/java/com/gonghak98/v2/certification/domain/LabCourseRule.java
+++
@@ -1,7 +0,0 @@
-package com.gonghak98.v2.certification.domain;
-
-import java.util.Set;
-
-public record LabCourseRule(Set<String> essentialLabCourseNames,
-                            int minimumCount) {
-}
