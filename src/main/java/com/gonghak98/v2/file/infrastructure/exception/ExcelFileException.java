7444423890355ea25eb776831657b1a5b4409fcb
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-09-06 10:15:18 +0900

    feat: 기이수 업로드 처리용 서비스 객체 코드 구현 및 테스트

---
+++ src/main/java/com/gonghak98/v2/file/infrastructure/exception/ExcelFileException.java
@@ -0,0 +1,8 @@
+package com.gonghak98.v2.file.infrastructure.exception;
+
+public class ExcelFileException extends RuntimeException {
+
+    public ExcelFileException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/gonghak98/v2/file/service/dto/FileData.java
@@ -0,0 +1,6 @@
+package com.gonghak98.v2.file.service.dto;
+
+public record FileData(int courseId, String courseName,
+                       int year, int semester) {
+
+}
