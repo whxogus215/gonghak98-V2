d496d3f1ce719ee712a45a4c5ff26fa61a4c9a40
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-09-10 14:01:49 +0900

    feat: 교양(전문교양) 생성하는 Factory 객체 구현

---
+++ src/main/java/com/gonghak98/v2/report/domain/abeek/exception/AbeekException.java
@@ -0,0 +1,8 @@
+package com.gonghak98.v2.report.domain.abeek.exception;
+
+public class AbeekException extends RuntimeException {
+
+    public AbeekException(String message) {
+        super(message);
+    }
+}
