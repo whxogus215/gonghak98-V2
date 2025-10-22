8091c76865a98aa73ac8753b0a3b02b4da5970dc
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-10-22 23:28:21 +0900

    refactor: 시큐리티 관련 설정 제거

--- src/main/java/com/gonghak98/v2/MainApplication.java
+++ src/main/java/com/gonghak98/v2/MainApplication.java
@@ -2,9 +2,8 @@ package com.gonghak98.v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
