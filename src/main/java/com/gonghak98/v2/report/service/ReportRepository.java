e94a00ee3679009c00f43b9fc4be074459d5c7a2
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-02-26 12:21:27 +0900

    refactor: 참조 안하는 import문 삭제

--- src/main/java/com/gonghak98/v2/report/service/ReportRepository.java
+++ src/main/java/com/gonghak98/v2/report/service/ReportRepository.java
@@ -2,7 +2,6 @@ package com.gonghak98.v2.report.service;
 
 import com.gonghak98.v2.report.controller.dto.ReportResponse;
 import com.gonghak98.v2.report.domain.abeek.dto.CheckResult;
-import com.gonghak98.v2.report.domain.abeek.dto.RequirementResult;
 
 public interface ReportRepository {
 
