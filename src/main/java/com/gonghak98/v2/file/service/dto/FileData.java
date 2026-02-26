677ddd3e8b5fe8e762a56238491c12a410bc45f4
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-02-26 13:21:22 +0900

    feat: 기이수 과목 데이터에 과목의 학점(성적X)이 담기도록 수정
    - 이수한 총 학점 계산 시, 필요한 데이터를 기이수 과목(CompletedCourse)에 담도록 수정
      - 엑셀 파일 처리 과정에서 기이수 성적 파일의 "학점" 컬럼 추출
      - 학점과 평점(A+, A0 등) 혼동 주의

--- src/main/java/com/gonghak98/v2/file/service/dto/FileData.java
+++ src/main/java/com/gonghak98/v2/file/service/dto/FileData.java
@@ -1,6 +1,9 @@
 package com.gonghak98.v2.file.service.dto;
 
-public record FileData(int courseId, String courseName,
-                       int year, int semester) {
+public record FileData(int courseId,
+                       String courseName,
+                       int year,
+                       int semester,
+                       double point) {
 
 }
