43d968a77bbda825a966bda815a645baf61053ba
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-03-04 11:25:25 +0900

    feat: 기이수 성적 파일 예외 발생 시, 에러 메시지 반환하는 기능 추가
    - 에러 코드, HTTP 상태 코드, 에러 메시지를 반환
    - 에러 코드는 내부적으로 관리

---
+++ src/main/java/com/gonghak98/v2/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.gonghak98.v2.common.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ExceptionResponse> handleExcelException(BaseException exception) {
+        final BaseExceptionType exceptionType = exception.exceptionType();
+        return ResponseEntity.status(exceptionType.httpStatus())
+                             .body(new ExceptionResponse(exceptionType.httpStatus().value(),
+                                                         exceptionType.errorCode(),
+                                                         exceptionType.errorMessage()));
+    }
+}
