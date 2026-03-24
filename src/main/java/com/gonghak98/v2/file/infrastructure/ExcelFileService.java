c768041203804e26eaba03739964b79e057ba895
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-03-24 13:44:21 +0900

    refactor: xls 확장자를 지원하는 Workbook 코드 삭제

--- src/main/java/com/gonghak98/v2/file/infrastructure/ExcelFileService.java
+++ src/main/java/com/gonghak98/v2/file/infrastructure/ExcelFileService.java
@@ -11,7 +11,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ooxml.POIXMLException;
 import org.apache.poi.openxml4j.util.ZipSecureFile;
 import org.apache.poi.ss.usermodel.DataFormatter;
@@ -57,6 +56,48 @@ public class ExcelFileService implements FileService {
         return extractData(worksheet, dataFormatter);
     }
 
+    //업로드 파일 검증
+
+    private void validateExcelFileFormat(MultipartFile file, String extension) throws ExcelFileException {
+        if (file.isEmpty()) {
+            throw new ExcelFileException(ExcelFileExceptionType.EMPTY_EXCEL_FILE);
+        }
+        if (file.getSize() > MAX_FILE_SIZE) {
+            throw new ExcelFileException(ExcelFileExceptionType.EXCEED_EXCEL_FILE_SIZE);
+        }
+        if (!extension.equals("xlsx")) { //엑셀파일이 아닐 때
+            throw new ExcelFileException(ExcelFileExceptionType.INVALID_EXCEL_FILE_TYPE);
+        }
+
+        try (InputStream is = file.getInputStream()) {
+            Tika tika = new Tika();
+            String mimeType = tika.detect(is);
+            if (!mimeType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                && !mimeType.equals("application/x-tika-ooxml")) {
+                throw new ExcelFileException(ExcelFileExceptionType.INVALID_EXCEL_FILE_TYPE);
+            }
+        } catch (IOException e) {
+            throw new ExcelFileException(ExcelFileExceptionType.RETRY_EXCEL_FILE);
+        }
+    }
+
+    private Sheet createWorkSheet(MultipartFile file, String extension) {
+        ZipSecureFile.setMinInflateRatio(0.01d);
+        ZipSecureFile.setMaxEntrySize(10L * 1024 * 1024); // 해당 값은 MAX_FILE_SIZE와 최대 압축률의 값을 고려해서 설정해야 합니다.
+        ZipSecureFile.setMaxTextSize(1_000_000L);
+
+        try (InputStream is = file.getInputStream()) {
+            return new XSSFWorkbook(is).getSheetAt(0);
+        } catch (IOException e) {
+            throw new ExcelFileException(ExcelFileExceptionType.RETRY_EXCEL_FILE);
+        } catch (POIXMLException e) {
+            throw new ExcelFileException(ExcelFileExceptionType.INVALID_EXCEL_FILE_TYPE);
+        } catch (Exception e) {
+            // TODO 추후 로그 남기기
+            throw new ExcelFileException(ExcelFileExceptionType.INVALID_EXCEL_FILE_TYPE);
+        }
+    }
+
     private FileResponse extractData(Sheet worksheet, DataFormatter dataFormatter) {
         List<FileData> fileDatas = new ArrayList<>();  // 저장할 엔티티 리스트 생성
 
@@ -76,52 +117,6 @@ public class ExcelFileService implements FileService {
             fileDatas.add(new FileData(courseId, courseName, year, semester, point));
         }
         return new FileResponse(fileDatas);
-    }
-
-    //업로드 파일 검증
-    private void validateExcelFileFormat(MultipartFile file, String extension) throws ExcelFileException {
-        if (file.isEmpty()) {
-            throw new ExcelFileException(ExcelFileExceptionType.EMPTY_EXCEL_FILE);
-        }
-        if (file.getSize() > MAX_FILE_SIZE) {
-            throw new ExcelFileException(ExcelFileExceptionType.EXCEED_EXCEL_FILE_SIZE);
-        }
-        if (!extension.equals("xlsx")) { //엑셀파일이 아닐 때
-            throw new ExcelFileException(ExcelFileExceptionType.INVALID_EXCEL_FILE_TYPE);
-        }
-
-        try (InputStream is = file.getInputStream()) {
-            Tika tika = new Tika();
-            String mimeType = tika.detect(is);
-            if (!mimeType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-                && !mimeType.equals("application/vnd.ms-excel")
-                && !mimeType.equals("application/x-tika-ooxml")) {
-                throw new ExcelFileException(ExcelFileExceptionType.INVALID_EXCEL_FILE_TYPE);
-            }
-        } catch (IOException e) {
-            throw new ExcelFileException(ExcelFileExceptionType.RETRY_EXCEL_FILE);
-        }
-    }
-
-    private Sheet createWorkSheet(MultipartFile file, String extension) {
-        ZipSecureFile.setMinInflateRatio(0.01d);
-        ZipSecureFile.setMaxEntrySize(10L * 1024 * 1024); // 해당 값은 MAX_FILE_SIZE와 최대 압축률의 값을 고려해서 설정해야 합니다.
-        ZipSecureFile.setMaxTextSize(1_000_000L);
-
-        try (InputStream is = file.getInputStream()) {
-            if (extension.equals("xlsx")) {
-                return new XSSFWorkbook(is).getSheetAt(0);
-            } else { // 확장자가 xls일 때
-                return new HSSFWorkbook(is).getSheetAt(0);
-            }
-        } catch (IOException e) {
-            throw new ExcelFileException(ExcelFileExceptionType.RETRY_EXCEL_FILE);
-        } catch (POIXMLException e) {
-            throw new ExcelFileException(ExcelFileExceptionType.INVALID_EXCEL_FILE_TYPE);
-        } catch (Exception e) {
-            // TODO 추후 로그 남기기
-            throw new ExcelFileException(ExcelFileExceptionType.INVALID_EXCEL_FILE_TYPE);
-        }
     }
 
     private int convertCourseId(String parsedCourseId) {
