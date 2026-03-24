e43a2b27367c108b9b1a7b44974ccd48cb338329
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-03-24 13:45:36 +0900

    refactor: 최대 업로드 크기 변경에 따른 에러 메시지 내용 수정
    - 50KB -> 30KB

--- src/main/java/com/gonghak98/v2/file/exception/ExcelFileExceptionType.java
+++ src/main/java/com/gonghak98/v2/file/exception/ExcelFileExceptionType.java
@@ -10,7 +10,7 @@ public enum ExcelFileExceptionType implements BaseExceptionType {
     INVALID_EXCEL_FILE_TYPE(HttpStatus.BAD_REQUEST, "올바른 기이수 성적 파일을 업로드해주세요."),
     EMPTY_EXCEL_FILE(HttpStatus.BAD_REQUEST, "기이수 성적 파일을 업로드해주세요."),
     RETRY_EXCEL_FILE(HttpStatus.BAD_REQUEST, "업로드 과정에서 오류가 발생했습니다. 다시 시도해주세요."),
-    EXCEED_EXCEL_FILE_SIZE(HttpStatus.PAYLOAD_TOO_LARGE, "업로드 파일 크기는 최대 50KB를 초과할 수 없습니다.");
+    EXCEED_EXCEL_FILE_SIZE(HttpStatus.PAYLOAD_TOO_LARGE, "업로드 파일 크기는 최대 30KB를 초과할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
