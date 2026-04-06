4092a7f373af10a014d8ae7d941ca42789ea76d7
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-04-06 11:16:06 +0900

    feat: 검사 결과를 JSON으로 저장하는 로직 제거
    - MongoDB 의존성 삭제
    - Report 삭제
    - API HTTP 메서드 변경 : POST -> GET
    
    refactor: 검사 결과를 JSON으로 저장하는 로직 제거
    
    - MongoDB 의존성 삭제
    - Report 삭제

--- src/main/java/com/gonghak98/v2/report/controller/ReportController.java
+++ src/main/java/com/gonghak98/v2/report/controller/ReportController.java
@@ -5,7 +5,7 @@ import com.gonghak98.v2.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +18,10 @@ public class ReportController {
 
     private final ReportService reportService;
 
-    @PostMapping
+    @GetMapping
     public ResponseEntity<ReportResponse> createReport(@RequestPart String departmentName,
                                                        @RequestPart MultipartFile file) {
-        ReportResponse response = reportService.createReport(departmentName, file);
+        ReportResponse response = reportService.getReport(departmentName, file);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/com/gonghak98/v2/report/service/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gonghak98.v2.report.service;
-
-import com.gonghak98.v2.report.domain.abeek.dto.CheckResult;
-import com.gonghak98.v2.report.infrastructure.collection.Report;
-
-public interface ReportRepository {
-
-    Report save(CheckResult checkResult);
-}
