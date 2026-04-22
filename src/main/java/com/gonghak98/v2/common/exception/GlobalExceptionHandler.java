996b17893632412b92864756afedeb90ebca57ea
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-04-22 23:46:24 +0900

    feat: 예외 타입별 에러 코드 포함한 에러 메시지를 반환하는 핸들러 로직 추가

--- src/main/java/com/gonghak98/v2/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gonghak98/v2/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.gonghak98.v2.common.exception;
 
+import com.gonghak98.v2.file.exception.ExcelFileExceptionType;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,29 @@ public class GlobalExceptionHandler {
                                                          exceptionType.errorMessage()));
     }
 
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    public ResponseEntity<ExceptionResponse> handleFileSizeLimitExceededException(FileSizeLimitExceededException exception) {
+        final BaseExceptionType exceptionType = ExcelFileExceptionType.EXCEED_EXCEL_FILE_SIZE;
+        return ResponseEntity.status(exceptionType.httpStatus())
+                             .body(new ExceptionResponse(exceptionType.httpStatus().value(),
+                                                         exceptionType.errorCode(),
+                                                         exceptionType.errorMessage()));
+    }
+
     @ExceptionHandler(TypeMismatchException.class)
     public ResponseEntity<ExceptionResponse> handleMissingParams(TypeMismatchException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                              .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.value(),
                                                          "TYPE_MISMATCH",
                                                          "요청이 올바른 형식이 아닙니다."));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
+        final BaseExceptionType exceptionType = CommonExceptionType.SERVER_ERROR;
+        return ResponseEntity.status(exceptionType.httpStatus())
+                             .body(new ExceptionResponse(exceptionType.httpStatus().value(),
+                                                         exceptionType.errorCode(),
+                                                         exceptionType.errorMessage()));
     }
 }
