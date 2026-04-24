ded20c3fd070b512b73c59daeedcc902be1ae6a7
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-04-24 23:33:43 +0900

    feat: 학과이름과 기이수 과목 코드로 공학인증 과목 조회하는 JPA 쿼리 메서드 추가 및 테스트
    - JPQL + Fetch Join을 활용한 3중 조인

--- src/test/java/com/gonghak98/v2/report/infrastructure/jpa/JpaGonghakCourseRepositoryTest.java
+++ src/test/java/com/gonghak98/v2/report/infrastructure/jpa/JpaGonghakCourseRepositoryTest.java
@@ -8,10 +8,11 @@ import com.gonghak98.v2.report.infrastructure.entity.CourseEntity;
 import com.gonghak98.v2.report.infrastructure.entity.DepartmentEntity;
 import com.gonghak98.v2.report.infrastructure.entity.GonghakCourseEntity;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -21,6 +22,7 @@ import org.springframework.test.context.ActiveProfiles;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JpaGonghakCourseRepositoryTest {
 
     private final String testDepartmentName = "전자정보통신공학과";
@@ -35,26 +37,29 @@ class JpaGonghakCourseRepositoryTest {
     private JpaGonghakCourseRepository jpaGonghakCourseRepository;
 
     private DepartmentEntity departmentEntity;
-    private CourseEntity courseEntity;
-    private GonghakCourseEntity gonghakCourseEntity;
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
         departmentEntity = new DepartmentEntity(testDepartmentName);
         jpaDepartmentRepository.save(departmentEntity);
 
-        courseEntity = new CourseEntity("001234", "테스트", 3.0);
-        jpaCourseRepository.save(courseEntity);
+        CourseEntity courseEntity = new CourseEntity("001234", "전문교양 과목", 3.0);
+        CourseEntity courseEntity2 = new CourseEntity("005678", "MSC 과목", 3.0);
 
-        gonghakCourseEntity = new GonghakCourseEntity(departmentEntity, AbeekType.GYOYANG, CourseType.ESSENTIAL, courseEntity);
+        jpaCourseRepository.save(courseEntity);
+        jpaCourseRepository.save(courseEntity2);
+
+        GonghakCourseEntity gonghakCourseEntity = new GonghakCourseEntity(departmentEntity, AbeekType.GYOYANG, CourseType.ESSENTIAL, courseEntity);
+        GonghakCourseEntity gonghakCourseEntity2 = new GonghakCourseEntity(departmentEntity, AbeekType.MSC, CourseType.ESSENTIAL, courseEntity2);
         jpaGonghakCourseRepository.save(gonghakCourseEntity);
+        jpaGonghakCourseRepository.save(gonghakCourseEntity2);
     }
 
-    @AfterEach
+    @AfterAll
     void tearDown() {
-        jpaGonghakCourseRepository.delete(gonghakCourseEntity);
-        jpaDepartmentRepository.delete(departmentEntity);
-        jpaCourseRepository.delete(courseEntity);
+        jpaGonghakCourseRepository.deleteAll();
+        jpaDepartmentRepository.deleteAll();
+        jpaCourseRepository.deleteAll();
     }
 
     @Test
@@ -68,5 +73,18 @@ class JpaGonghakCourseRepositoryTest {
 
         //then
         assertThat(gonghakCourses).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("학과이름과 과목 코드 리스트로 일치하는 공학인증 과목 리스트를 조회할 수 있다.")
+    void 기이수_과목_코드로_공학인증_과목_엔티티_조회() {
+        //given
+        List<String> courseCodes = List.of("001234", "005678");
+
+        //when
+        final List<GonghakCourseEntity> gonghakCourses = jpaGonghakCourseRepository.findAllByDepartmentNameAndCourseCodeIn(testDepartmentName, courseCodes);
+
+        //then
+        assertThat(gonghakCourses).hasSize(2);
     }
 }
