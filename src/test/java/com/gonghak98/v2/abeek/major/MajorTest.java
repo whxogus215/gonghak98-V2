b40f7cba29926b992208fd25269686e92d593f8a
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-04-25 00:10:40 +0900

    feat: Major 객체가 RuleType을 기준으로 세부 영역을 검사하도록 수정
    - 연관된 과목 반환 시, MAJOR 타입인 기이수 과목만 반환하도록 수정
    - GeneralMajor, LabMajor 삭제

--- src/test/java/com/gonghak98/v2/abeek/major/MajorTest.java
+++ src/test/java/com/gonghak98/v2/abeek/major/MajorTest.java
@@ -1,44 +1,39 @@
 package com.gonghak98.v2.abeek.major;
 
-import static com.gonghak98.v2.abeek.fixture.MajorFixture.createGeneralMajor;
-import static com.gonghak98.v2.abeek.fixture.MajorFixture.createLabMajor;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.gonghak98.v2.abeek.fixture.GivenObjectFixture;
+import com.gonghak98.v2.fake.TestRequirementRule;
 import com.gonghak98.v2.report.domain.abeek.AbeekType;
 import com.gonghak98.v2.report.domain.abeek.dto.AreaCheckResult;
 import com.gonghak98.v2.report.domain.abeek.major.Major;
+import com.gonghak98.v2.report.domain.abeek.rule.Rule;
 import com.gonghak98.v2.report.domain.student.CompletedCourse;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
-public class MajorTest {
+class MajorTest {
 
     @Nested
     class 전자정보통신공학과 {
 
         @DisplayName("전공영역은 설계, 실험, 일반, 선후수 세부요건을 검사할 수 있다.")
-        @CsvSource(value = {"005611, 디지털논리회로", "009658, 전기회로실험", "008076, 기초광학및실험", "999999, 전자소자공정실험", "009666, 마이크로컴퓨터실험"})
-        @ParameterizedTest
-        void 전공영역_검사(String courseCode, String studentLabCourseName) {
+        @Test
+        void 전공영역_검사() {
             //given
-            CompletedCourse completedCourse = CompletedCourse.builder().code(courseCode)
-                                                             .name(studentLabCourseName)
-                                                             .build();
-            Major major = new Major(createLabMajor(),
-                                    createGeneralMajor(),
-                                    45);
+            List<CompletedCourse> completedCourses = GivenObjectFixture.createCompletedCoursesWithThreeCredit(5, AbeekType.MAJOR);
+            List<Rule> rules = List.of(new TestRequirementRule());
+            Major major = new Major(rules, 3 * 5);
 
             AreaCheckResult areaCheckResult = GivenObjectFixture.createCheckResult();
 
             //when
-            major.checkAllCourses(List.of(completedCourse), areaCheckResult);
+            major.checkAllCourses(completedCourses, areaCheckResult);
 
             //then
-            assertThat(areaCheckResult.passResults().get(AbeekType.MAJOR)).isFalse();
+            assertThat(areaCheckResult.passResults().get(AbeekType.MAJOR)).isTrue();
         }
     }
 }
